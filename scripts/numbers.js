ec2fc0be915fb2982546336015f944ad20e86202
Destiny <destinygil0926@gmail.com>
2024-05-14 17:16:16 -0500

    first commit

--- scripts/numbers.js
+++ scripts/numbers.js
@@ -1,3 +1,18 @@
 "use strict"
 
 console.log("hello from inside numbers.js");
+
+let z = 5;
+console.log(z);
+
+z = z+15;
+console.log(z+15);
+z -= 8;
+console.log(z);
+
+let average= (26+44+72+86+112)/5;
+console.log(average);
+
+let l = 14;
+console.log(l++);
+
--- scripts/variables.js
+++ scripts/variables.js
@@ -1,3 +1,19 @@
 "use strict"
 
 console.log("hello from inside variables.js");
+
+let price = 100;
+
+price = 150;
+
+console.log(price);
+
+const number = 42;
+// const doesnt let you change the value of the variables, you would use const if you want the work to stay consistent
+number = 10;
+console.log(number);
+
+var x = 2;
+// one reason to to use var as best practice is due to not always getting a error message 
+x = 25
+console.log(x);
